116bde51fc3fcc8b578df9d3fb74fb51917c0a30
Michael Ashton <mykl.ashton@gmail.com>
2017-07-24 18:01:54 -0400

    Add working level dropdown-event listeners

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -14,23 +14,196 @@ export default class Nav extends Component {
     }
 
     handleMouseEnter(event) {
-        this.setState({isVisible: true})
+        event.target.children[0].className = "dropdown-box show"
     }
 
     handleMouseLeave(event) {
-        this.setState({isVisible: false})
+        event.target.children[0].className = "dropdown-box"
     }
 
     render() {
         return (
-            <div>
-                <h5 onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>v Dropdown v</h5>
-                <ul className={"dropdown-display" + (this.state.isVisible ? " show": "")}>
-                    <li>Item 1</li>
-                    <li>Item 2</li>
-                    <li>Item 3</li>
-                </ul>
-            </div>
+            <ul>
+                <li onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} >Clothing & Accessories
+                    <ul className="dropdown-box">
+                        <li>Accessories
+                            <ul>
+                                <li>Belts & Suspenders</li>
+                                <li>Gloves & Mittens</li>
+                                <li>Hair accessories
+                                    <ul>
+                                        <li>Fascinators & Mini Hats</li>
+                                        <li>Headbans</li>
+                                        <li>Ties & Elastics</li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>Bags & Purses
+                            <ul>
+                                <li>Test items</li>
+                                <li>More test items</li>
+                                <li>Some bullshit</li>
+                            </ul>
+                        </li>
+
+                        <li>Clothing
+                            <ul>
+                                <li>Test items</li>
+                                <li>More test items</li>
+                                <li>Some bullshit</li>
+                            </ul>
+                        </li>
+
+                        <li>Shoes
+                            <ul>
+                                <li>Test items</li>
+                                <li>More test items</li>
+                                <li>Some bullshit</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+                <li onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} >Jewelry
+                    <ul className="dropdown-box">
+                        <li>Subcategory 1
+                            <ul>
+                                <li>LI 1</li>
+                                <li>LI 2</li>
+                                <li>LI 3</li>
+                            </ul>
+                        </li>
+                        <li>Subcategory 2
+                            <ul>
+                                <li>LI 1</li>
+                                <li>LI 2</li>
+                                <li>LI 3</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+
+                <li onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} >Craft Supplies & Tools
+                    <ul className="dropdown-box">
+                        <li>Subcategory 1
+                            <ul>
+                                <li>LI 1</li>
+                                <li>LI 2</li>
+                                <li>LI 3</li>
+                            </ul>
+                        </li>
+                        <li>Subcategory 2
+                            <ul>
+                                <li>LI 1</li>
+                                <li>LI 2</li>
+                                <li>LI 3</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+
+
+                <li onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} >Weddings
+                    <ul className="dropdown-box">
+                        <li>Subcategory 1
+                            <ul>
+                                <li>LI 1</li>
+                                <li>LI 2</li>
+                                <li>LI 3</li>
+                            </ul>
+                        </li>
+                        <li>Subcategory 2
+                            <ul>
+                                <li>LI 1</li>
+                                <li>LI 2</li>
+                                <li>LI 3</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+
+                <li onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} >Entertainment
+                    <ul className="dropdown-box">
+                        <li>Subcategory 1
+                            <ul>
+                                <li>LI 1</li>
+                                <li>LI 2</li>
+                                <li>LI 3</li>
+                            </ul>
+                        </li>
+                        <li>Subcategory 2
+                            <ul>
+                                <li>LI 1</li>
+                                <li>LI 2</li>
+                                <li>LI 3</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+
+                <li onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} >Home & Living
+                    <ul className="dropdown-box">
+                        <li>Subcategory 1
+                            <ul>
+                                <li>LI 1</li>
+                                <li>LI 2</li>
+                                <li>LI 3</li>
+                            </ul>
+                        </li>
+                        <li>Subcategory 2
+                            <ul>
+                                <li>LI 1</li>
+                                <li>LI 2</li>
+                                <li>LI 3</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+                <li onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} >Kids & Baby
+                    <ul className="dropdown-box">
+                        <li>Subcategory 1
+                            <ul>
+                                <li>LI 1</li>
+                                <li>LI 2</li>
+                                <li>LI 3</li>
+                            </ul>
+                        </li>
+                        <li>Subcategory 2
+                            <ul>
+                                <li>LI 1</li>
+                                <li>LI 2</li>
+                                <li>LI 3</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+                <li onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} >Vintage
+                    <ul className="dropdown-box">
+                        <li>Subcategory 1
+                            <ul>
+                                <li>LI 1</li>
+                                <li>LI 2</li>
+                                <li>LI 3</li>
+                            </ul>
+                        </li>
+                        <li>Subcategory 2
+                            <ul>
+                                <li>LI 1</li>
+                                <li>LI 2</li>
+                                <li>LI 3</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         )
     }
 
