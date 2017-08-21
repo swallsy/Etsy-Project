92bc5c9c984e47efe90791b52d8ecc9389029ddd
Michael Ashton <mykl.ashton@gmail.com>
2017-08-21 16:29:59 -0400

    Updates

--- src/Home.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './components/Home/Home.js';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Home />, div);
-});
